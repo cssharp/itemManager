f612899e9ecf4c9d1aa722869faa7d598ca09b2d
edc <edc@edc.com>
2017-08-10 22:41:27 +0800

    txt 2 db

--- todb.py
+++ todb.py
@@ -3,7 +3,9 @@
 
 import sqlite3
 import json
+import os
 import sys
+import logging
 reload(sys)
 sys.setdefaultencoding('utf-8')
 
@@ -12,37 +14,56 @@ def test():
     conn = sqlite3.connect('db.sqlite3')
     print "Opened database successfully";
     c = conn.cursor()
-    cursor = c.execute('SELECT * FROM app_item')
-    for row in cursor:
-       print "ID = ", row[0]
-       print "NAME = ", row[1]
-       print "ADDRESS = ", row[2]
-       print "SALARY = ", row[3], "\n"
-
+    cursor = c.execute('delete from app_item')
     print "Table created successfully";
     conn.commit()
     conn.close()
 
 
-def insert(sql):
+def insert(filePath='C:\\Users\\XIA\\Desktop\\gs\\goodsSpider\\archive\\a2-by-aerosoles_Cool Cat.txt'):
+    strx = open(filePath, 'r').read()
+    j = json.loads(strx)
+
+    sql = 'insert into app_item(brand, iid, name, imgUrls, category, weight, sizes, gender, itemUrl, price, feature) values (?,?,?,?,?,?,?,?,?,?,?)'
+    
     conn = sqlite3.connect('db.sqlite3')
     print "Opened database successfully";
     c = conn.cursor()
-    c.execute(sql)
+    c.executemany(sql, [( j['brand'], j['id'], j['name'], "\n".join(j['imgUrls']), j['category'], j['weight'], "\n".join(j['sizes']), j['gender'], j['itemUrl'], j['price'], j.get('feature', ''))])
     print "Table created successfully";
     conn.commit()
     conn.close()
 
-def prepSql(filePath='C:\\Users\\XIA\\Desktop\\gs\\goodsSpider\\archive\\a2-by-aerosoles_Cool Cat.txt'):
-    strx = open(filePath, 'r').read()
-    j = json.loads(strx)
-    strx = 'insert into app_item(brand, iid, name, imgUrls, category, weight, sizes, gender, itemUrl, price, feature) values ("{brand}","{iid}","{name}","{imgUrls}","{category}","{weight}","{sizes}","{gender}","{itemUrl}","{price}","{feature}")'
-    strx = strx.format(brand = j['brand'],iid = j['id'],name = j['name'],imgUrls = "\n".join(j['imgUrls']),category = j['category'],weight = j['weight'],sizes = "\n".join(j['sizes']),gender = j['gender'],itemUrl = j['itemUrl'],price = j['price'],feature = j['feature'],featureTran = j['featureTrans'])
-    return strx
+
+def getFiles(pathStart, path='C:\\Users\\XIA\\Desktop\\gs\\goodsSpider\\archive'):
+    dirs = os.listdir(path)
+    result = []
+    for x in dirs:
+        if not os.path.isdir(x):
+            if pathStart == 'all':
+                result.append('{0}/{1}'.format(path, x))
+            elif x.startswith(pathStart):
+                result.append('{0}/{1}'.format(path, x))
+    return result
+
+
+
+def getAllFiles(path='C:\\Users\\XIA\\Desktop\\gs\\goodsSpider\\archive'):
+    dirs = os.listdir(path)
+    result = []
+    for x in dirs:
+        if not os.path.isdir(x):
+            result.append('{0}/{1}'.format(path, x))
+    return result
+
 
 if __name__ == '__main__':
-    sql = prepSql()
-    insert(sql)
-
+    test()
+    #files = getFiles('adidas')
+    files = getAllFiles()
+    total = len(files)
+    for i, filePath in enumerate(files):
+        logging.warning('[{0}/{1}] {2}'.format(i+1, total, filePath))
+        insert(filePath)
 
 
