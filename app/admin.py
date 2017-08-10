65e2218ba49412381b95143f56d98374d977b451
edc <edc@edc.com>
2017-08-10 21:18:01 +0800

    +table Item

---
+++ app/admin.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.contrib import admin
+from models import Item
+
+class ItemAdmin(admin.ModelAdmin):
+    list_display = ('name', 'brand', 'category', 'weight', 'price')
+    search_fields = ('name', 'brand')
+
+# Register your models here.
+admin.site.register(Item)
